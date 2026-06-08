4c685de9e88b026d3089f1f7848caac8f43fcb77
Juana Bellone <juanabellone@MacBookAir.fibertel.com.ar>
2026-06-08 16:47:35 -0300

    remove imapflow from next.config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['imapflow', 'mailparser'],
-  },
-}
+const nextConfig = {}
 
 module.exports = nextConfig
